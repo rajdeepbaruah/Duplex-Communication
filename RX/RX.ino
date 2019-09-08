0b6bb48019903a6a94fbd41a3b70f24761dfa420
RAJDEEP BARUAH <46004260+rajdeepbaruah@users.noreply.github.com>
2019-09-08 15:42:53 +0530

    Add files via upload

---
+++ RX/RX.ino
@@ -0,0 +1,104 @@
+
+
+//Rx
+
+
+#include <SPI.h>    // ( Net surf for SPI pins )
+#include <Ethernet.h>
+#include <EthernetUdp.h>
+#include<LiquidCrystal.h>
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(8, 9, 5, 4, 3, 2);
+//code start
+
+int pinButton = 14;
+int pinButton1= 15;
+
+int stateLED,stateLED1 = LOW;
+int stateButton,stateButton1;
+int previous,previous1 = LOW;
+long time = 0;
+long debounce = 100;
+int codeS1 = 2; //Codes must be even
+int codeS2 = 4;
+
+
+
+
+
+
+//Device IP & MAC (Variable)
+byte mac[] = { 0x2A, 0x00, 0x22, 0x22, 0x22, 0x44 }; //(Naming rules)
+IPAddress ip(192, 168, 1, 10);                       //(IP rules according to subnet/gateway)
+
+// -------- Do not change the section below -----------------
+const unsigned int localPort = 1369;    // Process ID port (TCP/UDP Alternate port = 8888)
+char remote_IP1[]="192.168.1.20";
+int remote_Port1 = 9631;
+char UDP_TX_Buffer[80];
+char recvdBuffer[UDP_TX_PACKET_MAX_SIZE + 1]; // Buffer for incoming data
+EthernetUDP Udp;  //Class variable (Initiates process of UDP)
+// ------------------------------------------------------------
+
+
+
+
+int temperature =atoi(recvdBuffer);
+int flamedet=0;
+
+int threshold =400;
+
+
+
+
+
+
+void setup()
+{
+ lcd.begin(16, 2);
+ lcd.clear();
+  lcd.print("Welcome");
+  delay(500);
+  lcd.setCursor(0, 1);
+ lcd.clear();
+ lcd.print("Manless Subtration");
+ lcd.setCursor(0,2);
+ lcd.print("Optical Fiber");
+ delay(2000);
+ //lcd.print(temperature);
+  pinMode(6,OUTPUT);
+  pinMode(pinButton, INPUT);
+  pinMode(pinButton1,INPUT);
+  pinMode(7,OUTPUT);
+  Ethernet.begin(mac, ip);   // Set up the Ethernet Shield
+  Udp.begin(localPort);      // Open a socket for this port
+  Serial.begin(9600);        // Set up serial monitor with PC4
+  delay(500);
+}
+char first;
+int i = 0, flag = 0;
+char text;
+
+
+
+void loop()
+{
+ 
+  //transmit
+int recvdSize = Udp.parsePacket();
+int count,count1,flag1;
+int Pressed = 0;
+int incomingByte = 0;
+
+ if (Udp.available()) {
+    IPAddress remote = Udp.remoteIP();
+    Udp.read(recvdBuffer, UDP_TX_PACKET_MAX_SIZE);
+            // Gets rid of the message header
+    
+    Serial.println(recvdBuffer);
+
+
+ }
+}
+
---
+++ TX/TX.ino
@@ -0,0 +1,123 @@
+
+//Tx
+ 
+ 
+#include <SPI.h>
+#include <Ethernet.h>
+#include <EthernetUdp.h>
+ 
+//PIN on board
+ 
+ 
+int pinButton = 4;
+int pinButton1= 5;
+int flame = 8;
+int LED = 12;
+int LED1 =13;
+int stateLED,stateLED1,stateLED2 = LOW;
+int stateButton,stateButton1,stateButton2;
+int previous,previous1,previous2 = LOW;
+long time = 0;
+long debounce = 100;
+ 
+ 
+ 
+//-------- system config -----------------
+byte mac[] = {0x2A, 0x00, 0x22, 0x22, 0x22, 0x22};
+IPAddress ip(192, 168, 1, 20);
+const unsigned int localPort = 9631;
+ 
+char remote_IP[] = "192.168.1.10";
+int remote_Port = 1369;
+ 
+char UDP_TX_Buffer[80]; // For future use...
+ 
+char recvdBuffer[UDP_TX_PACKET_MAX_SIZE + 1];
+//char UDP_RX_Buffer[80]; // For future use...
+EthernetUDP Udp;
+//-----------------------------------------
+ 
+int analogPin = 3;
+int firstpin=2;
+int reading;
+float tempC;
+ 
+void setup()
+{
+ 
+  Ethernet.begin(mac, ip);  // Set up the Ethernet Shield
+  Udp.begin(localPort);     // Open a socket for this port
+  Serial.begin(9600);       // Set up serial monitor with PC
+  //pinMode(13, OUTPUT);
+ analogReference(INTERNAL);
+ 
+pinMode(6,OUTPUT);
+pinMode(7,OUTPUT);
+pinMode(8,INPUT);
+   pinMode(pinButton, INPUT);
+   pinMode(pinButton1,INPUT);
+  pinMode(LED, OUTPUT);
+  pinMode(LED1,OUTPUT);
+ 
+ 
+  delay(100);
+ 
+ 
+}
+ 
+int newS1, oldS1 = 0, newS2, oldS2 = 0, newS3, oldS3 = 0, newS4, oldS4 = 0;
+ 
+int sData_code = 1;
+int codeS1 = 2; //Codes must be even
+int codeS2 = 4;
+int codeS3 = 6;
+int codeS4 = 8;
+ 
+ 
+void loop()
+{
+int recvdSize = Udp.parsePacket();
+ 
+reading = digitalRead(analogPin);
+tempC = reading;
+Serial.println(tempC);
+delay(100);
+ 
+ itoa(tempC, UDP_TX_Buffer, 10); 
+ 
+   Udp.beginPacket(remote_IP, remote_Port);
+ 
+   Udp.write(UDP_TX_Buffer);
+ 
+   Udp.endPacket();
+ 
+   strcpy(UDP_TX_Buffer,"");
+ 
+   delay(200);
+ 
+ 
+   
+  if (Udp.available()) {
+    IPAddress remote = Udp.remoteIP();
+    Udp.read(recvdBuffer, UDP_TX_PACKET_MAX_SIZE);
+    recvdBuffer[recvdSize] = '\0';
+    recvdSize -= 8; 
+ 
+ 
+    Serial.println(recvdBuffer);
+  
+ 
+  }
+}
+  
+  
+void remote_Send(int data)
+{
+  itoa(data, UDP_TX_Buffer, 10);
+  Udp.beginPacket(remote_IP, remote_Port); // REMOTE IP/Port
+  Udp.write(UDP_TX_Buffer);                         //Byte/string
+  Udp.endPacket();
+  delay(200);
+}
+ 
+
